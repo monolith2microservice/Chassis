c0e78017309c113d4670fb6deb173c983ff58dfb
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2025-10-18 15:07:17 +0200

    Fixed publish 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice_chassis",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "pika",
