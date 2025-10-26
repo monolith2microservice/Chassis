ee2344c3aa19efbdb1a4465e2af9843262773d93
enaut-genua <enaut.genua@alumni.mondragon.edu>
2025-10-26 20:24:22 +0100

    Add debugging

--- src/chassis/messaging/listener.py
+++ src/chassis/messaging/listener.py
@@ -42,7 +42,7 @@ class RabbitMQListener(RabbitMQBaseClient):
     ) -> MessageType:
         assert content_type == RabbitMQBaseClient._CONTENT_TYPE, "Only valid content should be processed."
         decoded_str = body.decode()
-        self._logger.debug(f"Queue: {self._queue} Received message: {decoded_str}")
+        self._logger.info(f"Queue: {self._queue} Received message: {decoded_str}")
         return json.loads(decoded_str)
 
     def consume(
